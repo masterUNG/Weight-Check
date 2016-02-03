99688901cd6a884741e0dd2f12babbed3a3c5994
masterUNG <phrombutr@gmail.com>
2016-02-03 10:29:35 +0700

    Create Constructor finish

--- app/src/main/java/appewtc/masterung/weightcheck/MyOpenHelper.java
+++ app/src/main/java/appewtc/masterung/weightcheck/MyOpenHelper.java
@@ -4,4 +4,8 @@ package appewtc.masterung.weightcheck;
  * Created by masterUNG on 2/3/16 AD.
  */
 public class MyOpenHelper {
-}
+
+    public MyOpenHelper() {
+    }   // Constructor
+
+}   // Main Class
