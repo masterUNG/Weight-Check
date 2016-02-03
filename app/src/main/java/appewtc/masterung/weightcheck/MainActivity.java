45a9a82bf80e4a09eab6bf3a97cb55d6af8d3e76
masterUNG <phrombutr@gmail.com>
2016-02-03 11:35:22 +0700

    Create SQLite Success Ful

--- app/src/main/java/appewtc/masterung/weightcheck/MainActivity.java
+++ app/src/main/java/appewtc/masterung/weightcheck/MainActivity.java
@@ -1,13 +1,22 @@
 package appewtc.masterung.weightcheck;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private MyManage objMyManage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Request Database
+        objMyManage = new MyManage(this);
+
+    }   // Main Method
+
+}   // Main Class
