87c0af3aed5a197b96860f885f8447a4d5af052f
masterUNG <phrombutr@gmail.com>
2016-02-10 10:08:39 +0700

    Check Regis Finish

--- app/src/main/java/appewtc/masterung/weightcheck/MainActivity.java
+++ app/src/main/java/appewtc/masterung/weightcheck/MainActivity.java
@@ -1,5 +1,8 @@
 package appewtc.masterung.weightcheck;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -17,6 +20,38 @@ public class MainActivity extends AppCompatActivity {
         //Request Database
         objMyManage = new MyManage(this);
 
+        //Check Register
+        checkRegister();
+
+
+
     }   // Main Method
 
+    private void checkRegister() {
+
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+
+        Cursor objCursor = objSqLiteDatabase.rawQuery("SELECT * FROM userTABLE", null);
+
+        if (objCursor.getCount() == 0) {
+
+            Intent objIntent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(objIntent);
+
+        }   //if
+        objCursor.close();
+
+    }   // checkRegister
+
 }   // Main Class
+
+
+
+
+
+
+
+
+
+
