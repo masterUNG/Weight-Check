48c446bcd746cf4c99d490d1694198e2fec76d68
masterUNG <phrombutr@gmail.com>
2016-02-17 12:10:10 +0700

    Finish 17Feb16

--- app/src/main/java/appewtc/masterung/weightcheck/MainActivity.java
+++ app/src/main/java/appewtc/masterung/weightcheck/MainActivity.java
@@ -5,12 +5,18 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private MyManage objMyManage;
-
+    private EditText userEditText, passwordEditText;
+    private Button loginButton;
+    private String userString, passwordString;
 
 
     @Override
@@ -18,15 +24,75 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Request Database
+        //Bind Widget คือการผูกตัวแปร ที่ประกาศ กับ Widget ที่อยู่ใน Layout
+        bindWidget();
+
+        //Connected Database
         objMyManage = new MyManage(this);
 
         //Check Register
         checkRegister();
 
-
+        //Button Controller
+        buttonController();
 
     }   // Main Method
+
+    private void buttonController() {
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value From Edit text to String
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (userString.equals("") || passwordString.equals("")) {
+                    //Have Space
+                    Toast.makeText(MainActivity.this, "Have Space", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    //No Space
+                    checkUser();
+
+                }
+
+            }   // event
+        });
+
+    }   // buttonController
+
+    private void checkUser() {
+
+        //การอ่านข้อมูลจาก SQLite
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor objCursor = objSqLiteDatabase.rawQuery("SELECT * FROM " + MyManage.table_user, null);
+        objCursor.moveToFirst();
+
+        String trueUser = objCursor.getString(1);
+        String truePassword = objCursor.getString(2);
+        String trueName = objCursor.getString(3);
+
+        if (userString.equals(trueUser)) {
+            //User OK
+
+        } else {
+            //User False
+            Toast.makeText(MainActivity.this, "User False", Toast.LENGTH_SHORT).show();
+        }
+
+    }   // checkUser
+
+    private void bindWidget() {
+
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+        loginButton = (Button) findViewById(R.id.button2);
+
+    }   //bindWidget
 
     private void checkRegister() {
 
