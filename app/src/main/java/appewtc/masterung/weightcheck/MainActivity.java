315b461c2d2c580e2269a508644f20f8c1da16ef
masterUNG <phrombutr@gmail.com>
2016-02-17 09:37:12 +0700

    Start 17Feb2016

--- app/src/main/java/appewtc/masterung/weightcheck/MainActivity.java
+++ app/src/main/java/appewtc/masterung/weightcheck/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private MyManage objMyManage;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
